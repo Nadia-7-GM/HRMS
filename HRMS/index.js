8ac7ef3f7f5f28cdc04a8c4f37d580a00df9ecf0
Nadia_GM <149421070+Nadia-7-GM@users.noreply.github.com>
2025-12-20 17:41:13 +0000

    updated

--- HRMS/index.js
+++ HRMS/index.js
@@ -1,7 +1,7 @@
 // console.log("✅ Node.js environment is working!");
 const express = require('express');
 const cors = require('cors');
-const pool = require('./db');
+const pool = require('./db').default;
 require('dotenv').config();
 
 const app= express();
@@ -13,14 +13,75 @@ const PORT = process.env.PORT || 3030;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 
-});
-//  get data from employee table
-app.get('/employees',async(req,res)=>{
-    try{
-        const result =await pool.query('Select * from employees');
-        res.json(result.rows);
-    }
-    catch(error){
-        res.status(500).json({err:error.message})
-    }
-});
+    });
+    //  get data from employee table
+    app.get('/employees',async(req,res)=>{
+        try{
+                const result =await pool.query('Select * from employees');
+                        res.json(result.rows);
+                            }
+                                catch(error){
+                                        res.status(500).json({err:error.message})
+                                            }
+                                            });
+    //  get data from department table
+    app.get('/departments',async(req,res)=>{
+        try{
+                const result =await pool.query('Select * from departments');
+                        res.json(result.rows);
+                            }
+                                catch(error){
+                                        res.status(500).json({err:error.message})
+                                            }
+                                            }   );
+ 
+             //  get data from countries table
+    app.get('/countries',async(req,res)=>{
+        try{
+                const result =await pool.query('Select * from countries');
+                        res.json(result.rows);
+                            }
+                                catch(error){
+                                        res.status(500).json({err:error.message})
+                                            }
+                                            }       );  
+                          //  get data from locations table
+    app.get('/locations',async(req,res)=>{
+        try{
+                const result =await pool.query('Select * from locations');
+                        res.json(result.rows);
+                            }
+                                catch(error){
+                                        res.status(500).json({err:error.message})
+                                            }
+                                            }     );    
+                                               //  get data from dependants table
+    app.get('/dependants',async(req,res)=>{
+        try{
+                const result =await pool.query('Select * from dependents');
+                        res.json(result.rows);
+                            }
+                                catch(error){
+                                        res.status(500).json({err:error.message})
+                                            }
+                                            }     );
+                                               //  get data from regions table
+    app.get('/regions',async(req,res)=>{
+        try{
+                const result =await pool.query('Select * from regions');
+                        res.json(result.rows);
+                            }
+                                catch(error){
+                                        res.status(500).json({err:error.message})
+                                            }
+                                            }     );
+                                                  //  get data from jobs table  
+    app.get('/jobs',async(req,res)=>{
+        try{
+                const result =await pool.query('Select * from jobs');
+                        res.json(result.rows);
+                            }
+                                catch(error){
+                                        res.status(500).json({err:error.message})
+                                            }
+                                            }     );
